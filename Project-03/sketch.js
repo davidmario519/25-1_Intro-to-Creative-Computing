33ba20b7d4f4d4872fc1fc6af71252bf62835d08
wan <davidmario519@gmail.com>
2025-04-01 00:53:12 +0900

    Project-03 완성

--- Project-03/sketch.js
+++ Project-03/sketch.js
@@ -1,8 +1,50 @@
+let particles = [];
+let num = 10000;
+
 function setup() {
-  createCanvas(400, 400);
+    createCanvas(windowWidth, windowHeight);
+    background(0);
+    noStroke();
+
+    for (let i = 0; i < num; i++) {
+        particles.push(createVector(random(width), random(height)));
+    }
 }
 
 function draw() {
-  background(220);
-  
+    colorMode(HSB);
+
+    ellipse(windowWidth / 2, windowHeight / 2, 70, 70);
+    
+    let delta = map(mouseX, 0, windowWidth, 0, 255);
+    fill(255 - delta, 95, 95, 100);
+    stroke(255 - delta, 95, 95, 100);
+
+    for (let i = 0; i < num; i++) {
+        let p = particles[i];
+        point(p.x, p.y);
+
+        let dx = map(mouseX, 0, windowWidth, 2, 200);
+        let dy = map(mouseY, 0, windowHeight, 2, 200);
+
+        p.x += (noise(p.x / 200, p.y / 200, 300000000) - 0.54) * dx;
+        p.y += (noise(p.x / 200, p.y / 200, 30000) - 0.5) * dy;
+    }
+}
+
+function mousePressed() {
+    let d = dist(mouseX, mouseY, windowWidth / 2, windowHeight / 2);
+    if (d < 35) {
+        background(0);
+
+        for (let i = 0; i < num; i++) {
+            particles[i] = createVector(random(width), random(height));
+        }
+    }
+}
+
+function keyPressed() {
+  if (key === 's' || key === 'S') {
+    saveCanvas('screenshot', 'png');
+  }
 }
